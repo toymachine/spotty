23c6a6f0d6990888c7a67b6dcf241251c1edd635
Henk Punt <henkpunt@gmail.com>
2012-04-05 14:16:22 +0200

    fetch a channel

--- src/spotty/handler/api.clj
+++ src/spotty/handler/api.clj
@@ -14,8 +14,8 @@
   (response/json (for [c (channel/all)] [(ds/key-id c) (:name c)])))
 
 (defpage "/api/channel/:id" {:keys [id]}
-  (if-let [channel (channel/get-by-id id)]
-    (response/json channel)
+  (if-let [channel (channel/get-by-id (Integer/parseInt id))]
+    (response/json {:name (:name channel)})
     (response/status 404 "Not found")))
 
 (defpage [:post "/api/channel"] {:keys [name description imageurl]}
