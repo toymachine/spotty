c1dc5e64fef8a56dd2fb48197d9308351eef42ca
Henk Punt <henkpunt@gmail.com>
2012-04-05 17:34:53 +0200

    chat

--- src/spotty/model/chat.clj
+++ src/spotty/model/chat.clj
@@ -1,6 +1,8 @@
 (ns spotty.model.chat
   (:use [clojure.string :only [lower-case trim]])
   (:require [appengine-magic.services.datastore :as ds]
+            [appengine-magic.services.channel :as chat-channel]
+            [spotty.model.channel :as channel]
             [clj-json.core :as json]
             [digest]))
 
@@ -10,5 +12,9 @@
   (ds/query :kind ChatMessage
             :filter (= :channel channel)))
 
-(defn send-message [creator channel msg]
-  (ds/save! (ChatMessage. creator channel (new java.util.Date) msg)))
+(defn send-message [sender channel msg]
+  (ds/save! (ChatMessage. sender channel (new java.util.Date) msg))
+  (let [sender-id (:spotify-id sender)]
+    (doseq [listener-id (channel/get-listeners)]
+      (when-not (= sender-id listener-id)
+        (chat-channel/send listener-id msg)))))
