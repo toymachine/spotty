8e29e218c8eb223cdaa326f3cc6f914e684de841
Henk Punt <henkpunt@gmail.com>
2012-04-06 11:39:08 +0200

    blaat

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject magic "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [noir "1.2.1"]
+                 [noir "1.3.0-alpha5"]
                  [digest "1.4.0"]]
   :dev-dependencies [[appengine-magic "0.5.0"]])
